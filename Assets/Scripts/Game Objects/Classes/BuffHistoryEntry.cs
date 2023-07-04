5a18258ac09f37ae0bad18c85bf9c3bb77e213cc
damolafolaranmi <neospacianaquadolphitler@gmail.com>
2023-07-04 21:43:50 +0100

    buff and debuff history
    keeping track of who debuffed/buffed what and where

---
+++ Assets/Scripts/Game Objects/Classes/BuffHistoryEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class BuffHistoryEntry
+{
+	CardLogic buffLogic;
+	string buffEffect;
+	int buffAmount;
+	CardLogic.Location buffLocation;
+
+	public BuffHistoryEntry(CardLogic cardLogic, string value, int amount, CardLogic.Location location)
+	{
+		buffLogic = cardLogic;
+		buffEffect = value;
+		buffAmount = amount;
+		buffLocation = location;
+	}
+}
+
---
+++ Assets/Scripts/Game Objects/Classes/DebuffHistoryEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class DebuffHistoryEntry
+{
+	CardLogic debuffLogic;
+	string debuffEffect;
+	int debuffAmount;
+	CardLogic.Location debuffLocation;
+
+	public DebuffHistoryEntry(CardLogic cardLogic, string value, int amount, CardLogic.Location location)
+	{
+		debuffLogic = cardLogic;
+		debuffEffect = value;
+		debuffAmount = amount;
+		debuffLocation = location;
+	}
+}
+
